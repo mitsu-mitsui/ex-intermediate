aac6418ba3b712b0b8d0f94793321ba75558941a
mitsu-mitsui <yuiko.mitsui@rakus.co.jp>
2020-06-22 15:34:25 +0900

    html: チーム名一覧表示

--- src/main/java/com/example/Controller/BaseBallController.java
+++ src/main/java/com/example/Controller/BaseBallController.java
@@ -1,14 +1,29 @@
 package com.example.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.Domain.BaseBallTeam;
+import com.example.Service.BaseBallTeamService;
 
 @Controller
 @RequestMapping("")
 public class BaseBallController {
-
+	@Autowired
+	private BaseBallTeamService baseBallTeamService;
+	
+	
 	@RequestMapping("/list")
-	public String inex() {
+	public String inex(Model model) {
+		
+		List<BaseBallTeam> baseBallTeams = baseBallTeamService.getAllBaseBallTeam();
+		
+		model.addAttribute("baseBallTeams", baseBallTeams);
+		
 		return "baseball-team-list";
 	}
 	
